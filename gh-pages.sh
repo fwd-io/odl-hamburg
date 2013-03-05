d1ff4f2e96aa916aedf91b68b604edfd32793950
Raffomania <raffomania@gmail.com>
2013-03-05 11:28:09 +0100

    minor typo in gh-pages.sh

--- gh-pages.sh
+++ gh-pages.sh
@@ -5,7 +5,7 @@ cd ./out && \
 remote_branch="gh-pages" && \
 git init && \
 git add . && \
-git commit -m'build' && \
+git commit -m 'build' && \
 git push $remote_repo master:$remote_branch --force && \
 rm -fr .git && \
 cd ../
