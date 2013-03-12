a1f9eeaabdc8e4fd66a0f1a06ff3e7e728e1be33
Raffomania <raffomania@gmail.com>
2013-03-12 14:28:02 +0100

    now uglifiyng all js files into one

--- src/documents/js/svg-fallback.js
+++ src/documents/js/svg-fallback.js
@@ -1,10 +1,8 @@
-<script>
-    if(!Modernizr.svg)
-    {
-        $("img").each(function() {
-            var oldSrc = $(this).attr("src");
-            var newSrc = "/assets/img/bitmaps" + oldSrc.slice(oldSrc.lastIndexOf("/"), oldSrc.lastIndexOf(".")) + ".png";
-            $(this).attr("src", newSrc);
-        });
-    }
-</script>
+if(!Modernizr.svg)
+{
+    $("img").each(function() {
+        var oldSrc = $(this).attr("src");
+        var newSrc = "/assets/img/bitmaps" + oldSrc.slice(oldSrc.lastIndexOf("/"), oldSrc.lastIndexOf(".")) + ".png";
+        $(this).attr("src", newSrc);
+    });
+}
